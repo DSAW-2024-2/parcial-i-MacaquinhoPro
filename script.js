7121cd735c2622f8a380862e0121a26ff4379460
MacaquinhoPro <andresriclo@unisabana.edu.co>
2024-08-28 08:55:51 -0500

    ParcialFinal

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add a click event listener to the share button
+document.getElementById('share-button').addEventListener('click', function() {
+    const shareButton = document.getElementById('share-button');
+    shareButton.classList.toggle('active');
+  });
+  
